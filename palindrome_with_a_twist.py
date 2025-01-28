7c4e7ce6aff64fe003f1e535e4387cca98709784
Sruthi Pedakolimi <40017897+Sruthi-Pedakolimi@users.noreply.github.com>
2025-01-28 11:57:53 -0600

    palindrome_with_a_twist

---
+++ palindrome_with_a_twist.py
@@ -0,0 +1,35 @@
+
+def checkSwappedPalindrome(left,right,s,index_mismatch):
+    #print(index_mismatch)
+    #Eprint(f"left: {left} and right: {right}")
+    if left >= right:
+        if len(index_mismatch) == 0:
+            return True
+        elif len(index_mismatch) == 1:
+            first = index_mismatch[0]
+            if s[first[0]] == s[len(s)//2] or s[first[1]] == s[len(s)//2]:
+                return True
+            return False
+        elif len(index_mismatch) == 2:
+            first = index_mismatch[0]
+            second = index_mismatch[1]
+            if s[first[0]] == s[second[1]] and s[first[1]] == s[second[0]] or \
+            s[first[0]] == s[second[0]] and s[first[1]] == s[second[1]]:
+                return True
+            return False
+    
+    if s[left] != s[right]:
+        #print("inside the left != right")
+        index_mismatch.append((left,right))
+        if len(index_mismatch) > 2:
+            return False
+    #print(index_mismatch)
+    return checkSwappedPalindrome(left+1,right-1,s,index_mismatch)
+    
+def canBecomePalindrome(s):
+    index_mismatch = []
+    return checkSwappedPalindrome(0,len(s)-1,s,index_mismatch)
+            
+if __name__ == "__main__":
+    s = input()
+    print("true" if canBecomePalindrome(s) else "false")

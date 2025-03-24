439ffdde861f2694a539ecebf651842989a848a2
ramya <sruthipedakolimi@gmail.com>
2025-03-24 01:17:46 -0500

    refactor: Improve logic in 035 Search Insert Position

--- 035_Search_Insert_Position.py
+++ 035_Search_Insert_Position.py
@@ -13,14 +13,14 @@ class Solution(object):
         n = len(nums)
         low = 0
         high = n - 1
-        insertion_index = 0
+        insertion_index = n
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
+            elif nums[mid] >= target:
                 high = mid - 1
+                insertion_index = mid
             else:
-                insertion_index = mid + 1
                 low = mid + 1
         return insertion_index
